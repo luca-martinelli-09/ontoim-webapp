76eb5b301ee8b62b31136537b160e1f3a3e1abfe
Luca Martinelli <martinelliluca98@gmail.com>
2022-10-04 16:50:18 +0200

    Added abstract

--- datareports/src/routes/about/+page.svelte
+++ datareports/src/routes/about/+page.svelte
@@ -1,8 +1,12 @@
 <article class="prose-lg">
   <h1>Informazioni sul progetto</h1>
 
+  <p>Questa applicazione è stata sviluppata da Luca Martinelli come parte del progetto di tesi di laurea magistrale in Computer Engineering presso l'Università degli Studi di Padova.</p>
+
+  <strong>Sommario</strong>
   <p>
-    Questo progetto è stato realizzato da Luca Martinelli come progetto di tesi di Laurea Magistrale in Computer Engineering presso l'Università degli Studi di Padova.
+    I Linked Open Data contribuiranno a trasformare l’eGovernment, consentendo alle pubbliche amministrazioni di collegare i propri dati ai dati di altri soggetti. Inoltre, la pubblicazione di Linked Open Data crea nuova conoscenza e incoraggia la creatività e l’innovazione. In questo modo, infatti, i governi possono attivare servizi più efficienti e intelligenti e le organizzazioni e i cittadini possono sviluppare nuove applicazioni e strumenti per lavorare, analizzare e dare un senso ai dati pubblici.<br>
+    In questo contesto, questa tesi presenterà la progettazione e lo sviluppo di un’ontologia per modellare i dati prodotti dai comuni. Inoltre, la tesi presenterà l’RDF Graph Builder, un software che mira a semplificare la pubblicazione di risorse sotto forma di Linked Open Data, e <i>Data Reports</i>, un’applicazione web per la visualizzazione e la consultazione dei Linked Open Data attraverso grafici, tabelle e mappe.
   </p>
 
   <div class="flex w-full max-w-xl">
